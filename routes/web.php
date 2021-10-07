a4d96885b6f7988855d3912bb72ee98800102ed7
YusrilIsmailAzi <60691798+Yusrilismailazi@users.noreply.github.com>
2021-10-07 22:02:41 +0700

    menyelesaikan vidio ke 4 laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Expr\PostDec;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [ 
+        "title" => "About",
         "name" => "Yusril Ismail Azi",
         "email" => "Aziyusril30@gamil.com",
         "image" => "aji.jpg"
@@ -27,7 +31,61 @@ Route::get('/about', function () {
 
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama" ,
+            "author" => "Yusril Ismail Azi",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores quo veritatis eos ipsam, nihil qui dolorem veniam, aliquam delectus tempore voluptatem repellendus provident, ea vel? Nesciunt nulla, assumenda culpa vero et illum dicta magnam eum minima sed eveniet. Placeat dolores non odio soluta quibusdam? Ut dolorum voluptate incidunt omnis sequi error aut dolores vitae tempore adipisci nostrum, ex deserunt provident repellendus, qui explicabo. Harum ea perspiciatis labore magni aspernatur, nemo blanditiis aut deserunt ullam voluptatem voluptates amet officia aliquam obcaecati?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-pertama" ,
+            "author" => "Adnan pidaus",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores quo veritatis eos ipsam, nihil qui dolorem veniam, aliquam delectus tempore voluptatem repellendus provident, ea vel? Nesciunt nulla, assumenda culpa vero et illum dicta magnam eum minima sed eveniet. Placeat dolores non odio soluta quibusdam? Ut dolorum voluptate incidunt omnis sequi error aut dolores vitae tempore adipisci nostrum, ex deserunt provident repellendus, qui explicabo. Harum ea perspiciatis labore magni aspernatur, nemo blanditiis aut deserunt ullam voluptatem voluptates amet officia aliquam obcaecati?"
+        ],
+    
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+
+    ]);
+});
+
+//halaman single post
+Route :: get('posts/{slug}' , function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama" ,
+            "author" => "Yusril Ismail Azi",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores quo veritatis eos ipsam, nihil qui dolorem veniam, aliquam delectus tempore voluptatem repellendus provident, ea vel? Nesciunt nulla, assumenda culpa vero et illum dicta magnam eum minima sed eveniet. Placeat dolores non odio soluta quibusdam? Ut dolorum voluptate incidunt omnis sequi error aut dolores vitae tempore adipisci nostrum, ex deserunt provident repellendus, qui explicabo. Harum ea perspiciatis labore magni aspernatur, nemo blanditiis aut deserunt ullam voluptatem voluptates amet officia aliquam obcaecati?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-pertama" ,
+            "author" => "Adnan pidaus",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores quo veritatis eos ipsam, nihil qui dolorem veniam, aliquam delectus tempore voluptatem repellendus provident, ea vel? Nesciunt nulla, assumenda culpa vero et illum dicta magnam eum minima sed eveniet. Placeat dolores non odio soluta quibusdam? Ut dolorum voluptate incidunt omnis sequi error aut dolores vitae tempore adipisci nostrum, ex deserunt provident repellendus, qui explicabo. Harum ea perspiciatis labore magni aspernatur, nemo blanditiis aut deserunt ullam voluptatem voluptates amet officia aliquam obcaecati?"
+        ],
+    
+    ];
+
+   $new_post = [];
+
+    foreach ($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+
+        }
+    }
+    return view('post' , [
+        "title"  => "Single Post",
+        "post" => $new_post 
+    ]);
+
 });
 
