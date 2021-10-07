d668ec8e50659703d04847d84bbaee200d39d51c
YusrilIsmailAzi <60691798+Yusrilismailazi@users.noreply.github.com>
2021-10-07 20:04:43 +0700

    Menyelesaikan vidio 3 laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', [ 
+        "name" => "Yusril Ismail Azi",
+        "email" => "Aziyusril30@gamil.com",
+        "image" => "aji.jpg"
+
+]);
+
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+
