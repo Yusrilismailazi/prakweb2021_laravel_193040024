b3bfe7d2797b9c1fc4b0cb736c87a7904473f7a8
YusrilIsmailAzi <60691798+Yusrilismailazi@users.noreply.github.com>
2021-11-23 00:57:37 +0700

    Laravel. Dashboard UI

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,22 +3,18 @@
 @section('container')
 <div class="container">
     <div class="row justify-content-center mb-5">
-        <div class="col-md-8">
-        <h2 class="mb-3">{{ $post->title }}</h2>
-<p>By. <a href="/posts?author={{ $post->author->username }}" 
-class="text-decoration-none">{{ $post->author->name }} </a> in
-<a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name  }}</a></p>
+      <div class="col-md-8">
+          <h1 class="mb-3">{{ $post->title }}</h1>
+          <p>By. <a class="text-decoration-none" href="/posts?author={{ $post->author->username }}">{{ $post->author->name }}</a> in <a class="text-decoration-none" href="/posts?category={{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
-<img src="https://source.unsplash.com/1200x400?{{$post->category->name}}" alt="{{ $post->category->name  }}" class="img-fluid">
+          <img class="img-fluid" src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}">
 
-<article class="my-3 fs-5">
-<h5>{!! $post->body !!}</h5>
-</article>
-
-
-<a href="/blog" class="d-block mt-3">Back to Posts</a>
-        </div>
+        <article class="my-3 fs-5">
+          {!! $post->body !!}
+        </article>
+        <a href="/posts" class="d-block mt-3 text-decoration-none" >Back to Posts</a>
+      </div>
     </div>
-</div>
+  </div>
 
 @endsection
