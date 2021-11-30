a5cf3b6369c243e89c1d2ba835278abc95ea28ac
YusrilIsmailAzi <60691798+Yusrilismailazi@users.noreply.github.com>
2021-11-30 15:18:22 +0700

    Laravel. Authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-
+use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       Paginator::useBootstrap();
+        Paginator::useBootstrap();
+
+        Gate::define('admin', function(User $user){
+            return $user->is_admin;
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
-
+// use App\Models\Post;
+// use App\Models\User;
 use App\Models\Category;
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\DashboardPostController;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -23,44 +25,69 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home",
+        "tittle" => "Home",
         'active' => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
-        "title" => "About",
+        "tittle" => "About",
         'active' => 'about',
-        "name" => "Muhammad Alwi Ramadhan",
-        "email" => "alwi.romadhon@gmail.com",
-        "image" => "alwi.jpeg"
+        "name" => "Yusril Ismail Azi",
+        "email" => "Aziyusril30@gmail.com",
+        "image" => "aji.jpg"
     ]);
 });
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/categories', function () {
+
+Route::get('/posts', [PostController::class, 'index']);
+
+// halaman single post
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function(){
     return view('categories', [
-        'title' => 'Post Categories',
+        'tittle' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
 
-//Login
+// middleware('guest') digunakan untuk user yang belum terauthenrifikasi
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
-//Register
+
+
+// Register
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
-//Dashboard
-Route::get('/dashboard', function() {
+
+// login dashboard
+Route::get('/dashboard', function(){
     return view('dashboard.index');
 })->middleware('auth');
 
-Route::get('dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth') ;
+// CRUD
+Route::get('/dashboard/posts/checkSlug',[DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
+// Admin Category
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
+// Route::get('/categories/{category:slug}',function(Category $category){
+//     return view('posts', [
+//         'tittle' => "Post by Category : $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('category', 'author')
+//     ]);
+// });
+
+// Route::get('/authors/{author:username}', function (User $author) {
+//     return view('posts', [
+//         'title' => "Post By Author : $author->name",
+//         'active' => 'author',
+//         'posts' => $author->posts->load('category', 'author')
+//     ]);
+// });
